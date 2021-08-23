4efb1628b8fa6a5a31367ea304f71b0a287be2b0
unknown <thukano0405@i.softbank.jp>
2021-08-24 05:59:09 +0900

    a

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+$(function() {
+    var today = new Date();
+    var y = today.getFullYear();
+    $('#mdp-demo').multiDatesPicker({
+        altField: '#altField',
+        changeMonth: true,
+        dateFormat: 'yy/mm/dd',
+        numberOfMonths: [1,2],
+        defaultDate: '-1m'
+    });
+});
